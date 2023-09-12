349b3377279cf2c725264cb599012bf4e55b4458
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-12 10:43:48 +0330

    Add files via upload

---
+++ ConsoleApp130/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp130
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            Console.WriteLine($"x squared:{Square(x)}");
+            Console.ReadLine();
+        }
+
+        static int Square(int y)
+        {
+            return y * y;
+        }
+
+    }
+}
